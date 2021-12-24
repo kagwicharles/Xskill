5b8802d827cc1f1a41894e54da8bda2fb52a3ab2
kagwicharles <ckagwi8@gmail.com>
2021-12-24 21:35:48 +0300

    Code refactor

--- app/src/main/java/com/example/co_leaner/ui/BottomSheet.kt
+++ app/src/main/java/com/example/co_leaner/ui/BottomSheet.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.co_leaner.R
+import com.example.co_leaner.databinding.BottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomSheet : BottomSheetDialogFragment() {
+
+    private var _binding: BottomSheetBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        val view = inflater.inflate(R.layout.bottom_sheet, container, false)
-        return view
+        _binding = BottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
