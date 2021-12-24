bf739c290c0685a75e34791343858e638fad66f6
kagwicharles <ckagwi8@gmail.com>
2021-12-24 21:19:47 +0300

    Code refactor

--- app/src/main/java/com/example/co_leaner/ui/MainActivity.kt
+++ app/src/main/java/com/example/co_leaner/ui/MainActivity.kt
@@ -5,29 +5,29 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.co_leaner.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.co_leaner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val tag: String = MainActivity::class.java.name
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.toolbar)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
         val navController = navHostFragment?.navController
         Log.d(tag, navController.toString())
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val bottomNav = binding.bottomNavigationView
         if (navController != null) {
             bottomNav.setupWithNavController(navController)
         }
